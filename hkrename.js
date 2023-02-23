90e6a189b6cc0b0a1c11b649a966ba55fb071037
Saw1g88 <103700462+Saw1g88@users.noreply.github.com>
2023-02-23 20:22:55 +0800

    Create hkrename.js

---
+++ hkrename.js
@@ -0,0 +1,15 @@
+function operator (proxies) {
+
+      const counter = {};
+
+      return proxies.map(p => {
+
+        if (!counter [香港]) counter [香港] = 0;
+
+        p.name = p. name + " " + (++counter [香港]).toString();
+
+        return p;
+
+      });
+
+  }
