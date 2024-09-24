a3f52fe663309a9c5cfbadf641f499927e35baf4
Saw <103700462+Saw1g88@users.noreply.github.com>
2024-09-24 11:24:45 +0800

    Update system.sh

--- Linux/system.sh
+++ Linux/system.sh
@@ -48,6 +48,7 @@ check_and_install wget
 if ! command -v docker &> /dev/null; then
     echo "安装 Docker..."
     wget -qO- https://get.docker.com | bash -s docker
+    echo "Docker 安装完成。"
 else
     echo "Docker 已安装，跳过安装。"
 fi
@@ -55,29 +56,31 @@ fi
 # 开启 TCP Fast Open (TFO)
 current_tfo_setting=$(cat /proc/sys/net/ipv4/tcp_fastopen)
 if [ "$current_tfo_setting" == "3" ]; then
-    echo "TCP Fast Open (TFO) 已开启，跳过。"
+    echo "TCP Fast Open 已开启，跳过设置。"
 else
-    echo "开启 TCP Fast Open (TFO)..."
+    echo "开启 TCP Fast Open..."
     echo "3" > /proc/sys/net/ipv4/tcp_fastopen
     echo "net.ipv4.tcp_fastopen=3" > /etc/sysctl.d/30-tcp_fastopen.conf
     sysctl --system
+    echo "已开启 TCP Fast Open。"
 fi
 
 # 配置 BBR 和 FQ
 current_qdisc=$(sysctl net.core.default_qdisc | awk '{print $3}')
 current_congestion_control=$(sysctl net.ipv4.tcp_congestion_control | awk '{print $3}')
 if [ "$current_qdisc" == "fq" ] && [ "$current_congestion_control" == "bbr" ]; then
-    echo "BBR 和 FQ 已开启，跳过。"
+    echo "bbr fq 已开启，跳过设置。"
 else
-    echo "配置 BBR 和 FQ..."
+    echo "配置 bbr..."
     echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
     echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
     sysctl -p
+    echo "已配置 BBR 和 FQ。"
 fi
 
 # 调整 DNS
 adjust_dns() {
-
+    echo "检查当前 DNS 配置..."
     if [ -f /etc/resolv.conf ]; then
         echo "备份当前的 /etc/resolv.conf 文件到 /etc/resolv.conf /etc/resolv.conf.bak..."
         cp /etc/resolv.conf /etc/resolv.conf.bak
@@ -103,10 +106,38 @@ dns_check() {
 # 执行 DNS 检查和调整
 dns_check
 
+# 检查和设置虚拟内存（Swap）
+check_and_setup_swap() {
+    swap_file="/swapfile"
+    
+    # 检查是否已经存在 swap
+    if swapon --show | grep -q "$swap_file"; then
+        echo "Swap 已设置，跳过设置。"
+        return
+    fi
+
+    echo "正在设置 Swap..."
+    
+    # 创建一个1GB的交换文件
+    sudo fallocate -l 1G "$swap_file" || sudo dd if=/dev/zero of="$swap_file" bs=1G count=1
+    sudo chmod 600 "$swap_file"
+    sudo mkswap "$swap_file"
+    sudo swapon "$swap_file"
+
+    # 更新 /etc/fstab 以在重启时自动挂载 swap
+    echo "$swap_file none swap sw 0 0" | sudo tee -a /etc/fstab
+
+    echo "Swap 已设置为 1024MB。"
+}
+
+# 执行虚拟内存检查和设置
+check_and_setup_swap
+
 # 设置时区
 set_timezone() {
     echo "设置时区为 Asia/Shanghai..."
     timedatectl set-timezone Asia/Shanghai
+    echo "时区已设置为 Asia/Shanghai。"
 }
 
 # 检查时区是否已经是 Asia/Shanghai
