b9d124ced8e9a67a4ca3ad610ec13ec5a4e669ef
Saw <103700462+Saw1g88@users.noreply.github.com>
2025-01-23 14:31:09 +0800

    Update docker_backup.sh

--- Linux/docker_backup.sh
+++ Linux/docker_backup.sh
@@ -6,7 +6,7 @@ read -p "请输入服务器标识(例如: oracle): " SERVER_ID
 # 备份脚本路径
 BACKUP_SCRIPT="/root/docker_backup.sh"
 
-# 创建备份脚本内容
+# 创建备份脚本
 cat > "$BACKUP_SCRIPT" << EOF
 #!/bin/bash
 backup_src="/opt/docker"
@@ -23,7 +23,7 @@ EOF
 # 赋予脚本执行权限
 chmod +x "$BACKUP_SCRIPT"
 
-# 直接使用 sudo 添加定时任务
-sudo bash -c "crontab -l 2>/dev/null | { cat; echo '0 1 * * 1 /bin/bash $BACKUP_SCRIPT'; } | crontab -"
+# 直接编辑 crontab
+(crontab -l 2>/dev/null; echo "0 1 * * 1 $BACKUP_SCRIPT") | crontab -
 
 echo "备份脚本安装完成"
