cd2320b83bad73a9056f33b6db31ac8adc6c58f1
Saw <103700462+Saw1g88@users.noreply.github.com>
2025-01-23 14:58:10 +0800

    Update docker_backup.sh

--- Linux/docker_backup.sh
+++ Linux/docker_backup.sh
@@ -14,17 +14,25 @@ BACKUP_SCRIPT="/root/docker_backup.sh"
 # 创建备份脚本
 cat > "$BACKUP_SCRIPT" << EOF
 #!/bin/bash
+
+# 定义颜色变量
+GREEN='\033[0;36m'
+RED='\033[0;31m'
+NC='\033[0m'
+
 # 定义变量
 backup_src="/opt/docker"
 backup_dest="onedrive:vps/${SERVER_ID}/docker"
 timestamp=\$(date +%Y%m%d_%H%M%S)
 
+# 在容器内执行备份操作
 docker exec rclone /bin/sh -c "\
     cd / && \
     tar czvf /tmp/backup_\${timestamp}.tar.gz \$backup_src && \
     rclone copy /tmp/backup_\${timestamp}.tar.gz \$backup_dest && \
     rm /tmp/backup_\${timestamp}.tar.gz"
 
+# 检查执行结果
 if [ \$? -eq 0 ]; then
     echo -e "\${GREEN}备份成功！备份文件：backup_\${timestamp}.tar.gz\${NC}"
 else
